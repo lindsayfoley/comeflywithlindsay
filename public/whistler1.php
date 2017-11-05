f0d2deaadb58ecd91dbfacd015ca26c13e0494a1
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-05 14:23:12 +0000

    - Adding to the copy and updated img alt text and captions

--- public/whistler1.php
+++ public/whistler1.php
@@ -19,33 +19,34 @@
 					<h1>Whistler Village, Canada</h1>
 					<p>Hiking, biking, skiing... definitely a must stop destination for all adventure seekers. Whistler is north of Vancouver and is where the 2010 Vancouver Winter Olympics were hosted, it's also home to Whistler Blackcomb which is a very popular ski resort.</p>
 					<figure>
-						<img src="images/whistler/summit-lodge-view-from-balcony-whistler.jpg" alt="The lovely town of Banff">
-						<figcaption>summit-lodge-view-from-balcony-whistler</figcaption>
+						<img src="images/whistler/whistler-village-autumnal-colours.jpg" alt="Autumnal coloured trees in Whistler Village, Canada">
+						<figcaption>Loved the autumnal colours in Whistler Village</figcaption>
 					</figure>
 					
-					<p>We travelled to Whistler from Vancouver International Airport on the Whistler Shuttle which took about 2.5 hours. The Whistler shuttles depart from the airport every hour so it's a great choice if you don't have a car. The views on the drive are just beautiful, you pass through Downtown Vancouver, Stanley Park and then have the ocean, trees and glacier covered mountains beside you. Whistler village is a pedestrianised, quaint, little town and has the most amazing red and autumnal coloured trees. In comparison to Banff, it feels smaller and more compact. There are shops, restaurants, bars and hotels and you can explore the whole village on foot in about 20 minutes.</p>
+					<p>We travelled to Whistler from Vancouver International Airport on the Whistler Shuttle which took about 2.5 hours. The Whistler shuttles depart from the airport every hour so it's a great choice if you don't have a car. The views on the drive are just beautiful, you pass through Downtown Vancouver, Stanley Park and then have the ocean, trees and glacier covered mountains beside you. Whistler village is a pedestrianised, quaint, little town and has the most amazing red and autumnal coloured trees. In comparison to Banff, it feels smaller and more compact. There are shops, restaurants, bars and hotels and you can explore the whole village on foot in about 20 minutes. We stayed at Summit Lodge Boutique Hotel located in the heart of the village.</p>
 					<figure>
-						<img src="images/whistler/whistler-village-autumnal-colours.jpg" alt="Moose Hotel and Suites, Banff">
-						<figcaption>whistler-village-autumnal-colours</figcaption>
+						<img src="images/whistler/summit-lodge-view-from-balcony-whistler.jpg" alt="The view from our balcony showing beautiful autumnal trees at Summit Lodge Boutique Hotel in Whistler, Canada">
+						<figcaption>View from our balcony at Summit Lodge Boutique Hotel</figcaption>
 					</figure>
 					
 					<p>We attempted to do the Rainbow trail but the Rainbow lake was just too far for us to make it to. We did manage to get to Alta Lake and Rainbow Falls which was really cool but let me warn you, a longggg walk for those who aren't fit; about a 3 hour roundtrip journey.</p>
 					<figure>
-						<img src="images/whistler/rainbow-falls-whistler.jpg" alt="Bow River trail in Banff, Canada">
-						<figcaption>rainbow-falls-whistler</figcaption>
+						<img src="images/whistler/rainbow-falls-whistler.jpg" alt="Hiking to Rainbow Falls in Whistler, Canada">
+						<figcaption>Hiking to Rainbow Falls in Whistler</figcaption>
 					</figure>					
 					
-					<p>The Lost Lake trail was a nice easy one to do from Whistler Village.</p>
+					<p>The Lost Lake trail was a nice easy one to do from Whistler Village.</p>					
 					<figure>
-						<img src="images/whistler/lost-lake-whistler-behind-trees.jpg" alt="Hiking in Tunnel Mountain in the snow, Banff - Alberta, Canada">
-						<figcaption>lost-lake-whistler-behind-trees</figcaption>	
-					</figure>						
+						<img src="images/whistler/lost-lake-whistler.jpg" alt="The Lost Lake in Whistler, Canada">
+						<figcaption>The Lost Lake in Whistler</figcaption>
+					</figure>		
 					
 					<p>Unfortunately for us, we arrived during off season which meant most things were closed.</p>
 					<figure>
-						<img src="images/whistler/lost-lake-whistler.jpg" alt="Lake Louise surrounded by snowy trees in Banff - Alberta, Canada">
-						<figcaption>lost-lake-whistler</figcaption>
-					</figure>		
+						<img src="images/whistler/lost-lake-whistler-behind-trees.jpg" alt="The Lost Lake behind the trees in Whistler, Canada">
+						<figcaption>The Lost Lake behind the trees in Whistler</figcaption>	
+					</figure>	
+					
 					<p>We stayed in Whistler for 3 nights in October 2017 and I would say this is the perfect amount of time. When I return, I'd like to do the activities that were closed, try out the gondola.</p>			
 					<hr>
 					
