411aedfb649bb7f7dacf7372d15244d494b43675
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-06 22:41:24 +0000

    - favicon and icon added - mail chimp sign up form added with custom CSS - copy tweaks

---
+++ public/structure/sign-up-form.php
@@ -0,0 +1,13 @@
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+	<form action="https://comeflywithlindsay.us17.list-manage.com/subscribe/post?u=b227b8206a4a9866eab30d0f7&amp;id=92b146d66d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	    <div id="mc_embed_signup_scroll">
+		<label for="mce-EMAIL">Get notified about new guides:	</label>
+		<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b227b8206a4a9866eab30d0f7_92b146d66d" tabindex="-1" value=""></div>
+	    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+	    </div>
+	</form>
+</div>
+
