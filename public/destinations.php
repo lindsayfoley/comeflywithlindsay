70ce7c270f8d9b21e2b5bc17f711361e1318ae8c
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-18 15:08:39 +0000

    - New Calgary page added - Organised images on destinations page - Calgary added to destinations page

--- public/destinations.php
+++ public/destinations.php
@@ -18,7 +18,16 @@
 				<article>
 					<p id="intro">I've created these travel guides to provide you with tips and advice on your upcoming holidays as well to inspire you about some incredible destinations that you may not have even considered visiting!</p>
 					<div class="row">
-						<img class="wide column" src="images/snow-covered-trees-mountains-banff-canada-wide.jpg" alt="Winter wonderland, snow topped mountains and trees in Banff - Alberta, Canada">
+						<img class="wide column" src="images/destinations/olympic-plaza-calgary-wide.jpg" alt="Olympic Plaza in Downtown Calgary - Alberta, Canada">
+						<div class="large-9 medium-10 column">
+							<h2><a href="/holiday-guides/calgary">Downtown Calgary, Canada</a></h2>
+							<p>Spending a day or two in Downtown Calgary before heading to Banff? Then this is the guide for you!</p>
+							<a class="button small" href="/holiday-guides/calgary">Read More About Calgary</a>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<img class="wide column" src="images/destinations/snow-covered-trees-mountains-banff-canada-wide.jpg" alt="Winter wonderland, snow topped mountains and trees in Banff - Alberta, Canada">
 						<div class="large-9 medium-10 column">
 							<h2><a href="/holiday-guides/banff">Banff National Park, Canada</a></h2>
 							<p>Part of the Rocky Mountains, you'll see the most amazing scenery; snowy mountains, fir trees and lakes.</p>
@@ -27,7 +36,7 @@
 					</div>
 					<hr>
 					<div class="row">
-						<img class="wide column" src="images/autumnal-colours-whistler-british-columbia-wide.jpg" alt="Stunning autumnal coloured trees in Whistler - British Columbia, Canada">
+						<img class="wide column" src="images/destinations/autumnal-colours-whistler-british-columbia-wide.jpg" alt="Stunning autumnal coloured trees in Whistler - British Columbia, Canada">
 						<div class="large-9 column medium-10">
 							<h2><a href="/holiday-guides/whistler">Whistler Village, Canada</a></h2>
 							<p>A cool little village, 2 hours drive from Vancouver Airport with a focus on sports and outdoor activities.</p>
@@ -36,7 +45,7 @@
 					</div>
 					<hr>
 					<div class="row">
-						<img class="wide column" src="images/port-view-stanley-park-vancouver-wide.jpg" alt="A view of the port and boats taken in Stanley Park">
+						<img class="wide column" src="images/destinations/port-view-stanley-park-vancouver-wide.jpg" alt="A view of the port and boats taken in Stanley Park">
 						<div class="large-9 medium-10 column">
 							<h2><a href="/holiday-guides/vancouver">Downtown Vancouver, Canada</a></h2>
 							<p>A must for those who enjoy city breaks and plenty of walking! Downtown Van has loads to see and do.</p>
---
+++ public/holiday-guides/calgary.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+	<title>Travel Guide On Downtown Calgary, Canada | Come Fly With Lindsay</title>
+	<?php include('../structure/meta.php'); ?>
+	<meta name="description" content="My inside guide to spending a couple of nights in Downtown Calgary in Alberta, Canada. Blog post by Come Fly With Lindsay">
+	<?php include('../structure/styles.php'); ?>
+	<?php include('../structure/icons.php'); ?>
+  </head>
+  
+  <body id="blog-post">
+	  
+	<?php include('../structure/header.php'); ?>
+	
+	<section>
+		<main class="row">
+			<div class="large-12 column">
+				<article class="large-12 column">
+					<h1>Downtown Calgary, Canada</h1>
+					<p>There's not a whole lot to do in Downtown Calgary, it seems to be more of a town that people pass through on their way to visit <a href="holiday-guides/banff">Banff</a> to see the Canadian Rocky mountains. I've put together a concise guide on what to do if you're there for a day or two.</p>
+					
+					<p>We stayed at the <a target="_blank" href="http://www.fairmont.com/palliser-calgary/">Fairmont Palliser Hotel</a> as the room price was incredibly reasonable for such a luxury hotel chain! The location is ideal, as it's in the heart of the city and just a few minutes walk from the Calgary Tower which you can probably see from your hotel room window. This hotel is grand, boasting a lavish foyer with elegant chandeliers, striking marble pillars and rich gold accents.</p>
+					
+					<figure>
+						<img src="/images/calgary/fairmont-palliser-lobby-downtown-calgary.jpg" alt="The lobby at the Fairmont Palliser in Downtown Calgary">
+						<figcaption>The lobby at the Fairmont Palliser in Downtown Calgary</figcaption>
+					</figure>
+	
+					<p>Our room was generous in size, especially for a standard and to my delight it was recently refurbished. It was pretty quiet so we slept fairly well, apart from hearing the odd few trains passing through. We ordered room service a few times when suffering from jet-lag and I have to say the the food was consistently outstanding, it was pricey but well worth it! The facilities include an indoor pool, spa and a Starbucks located on the lower ground floor which was useful when we needed water, snacks and of course hot drinks to warm us up.</p>
+					<figure>
+						<img src="/images/calgary/fairmont-queen-room-fairmont-palliser.jpg" alt="The Fairmont Queen room at the Fairmont Palliser">
+						<figcaption>The Fairmont Queen room at the Fairmont Palliser</figcaption>
+					</figure>					
+					
+					<p>We walked down to the Olympic Plaza on Stephen Avenue, where medals were presented in the 1988 Winter Olympics. There were no events on when we visited but it was still a nice place for a stroll on a sunny day. The plaza is well maintained and has a water feature in the center with the Canadian Maple leaf, surrounded by small fountains. Definitely worth a pit-stop, but don't go out of your way to visit.</p>
+					<figure>
+						<img src="/images/calgary/olympic-plaza-calgary.jpg" alt="Olympic Plaza in Downtown Calgary">
+						<figcaption>Olympic Plaza in Downtown Calgary</figcaption>
+					</figure>
+										
+					<p>We went to <a target="_blank" href="http://www.calgarytower.com/">Calgary Tower</a> one evening for dinner and to checkout the views of the city from the observation deck. We chose to eat at the revolving restaurant, <a target="_blank" href="http://www.sky360.ca/">Sky 360</a> which was a definitely a unique dining experience with pleasant food and excellent service. Top tip... don't leave your handbag on the window ledge, I learned the hard way that this doesn't rotate with you!</p>
+					<figure>
+						<img src="/images/calgary/sky-360-restaurant-calgary-tower.jpg" alt="The observation deck in Calgary Tower">
+						<figcaption>Don't look down... the observation deck in Calgary Tower</figcaption>
+					</figure>					
+					
+					<p>We stayed in Calgary for 2 nights in October 2017, over the Canadian Thanksgiving period (second Monday in October) which may be the reason why it was tremendously quiet. That said, I wouldn't plan to stay here for an extended period of time, a weekend would do!</p>			
+					<hr>
+					
+					<h2>My Top Tips</h2>
+					<ul>
+						<li>Dinner with a difference - if you're thinking of booking <a target="_blank" href="http://www.sky360.ca/">Sky 360</a> for dinner, time your reservation at sunset for a spectacular view</li>
+						<li>Wrap up warm - it was snowing in early October and was bitterly cold! Coats, scarves, gloves, hats are a must</li>
+						<li>Time well spent - if you're heading to Banff, consider extending your stay to spend more time there and less time in Calgary</li>
+					</ul>
+					<hr>
+					<div class="large-12 column text-right">
+						<a class="button small" href="../holiday-guides/banff">View Banff Guide</a>
+					</div>				
+				</article>
+			</div>
+		</main>
+	</section>
+	
+	<?php include('../structure/footer.php'); ?>
+	
+  </body>
+  <?php include('../structure/scripts.php'); ?>
+</html>
