2385093e0c47386b2c15a9d73257b47f1e691722
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-18 16:31:26 +0000

    Starting work on this new page

---
+++ public/holiday-guides/afternoon-tea-london.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+	<title>Guide To London's Afternoon Tea Scene, England UK | Come Fly With Lindsay</title>
+	<?php include('../structure/meta.php'); ?>
+	<meta name="description" content="I've been to so many lovely traditional afternoon teas in my home city of London, England. Here are my top 5 venues including Fortum and Mason and The Ritz">
+	<?php include('../structure/styles.php'); ?>
+	<?php include('../structure/icons.php'); ?>
+  </head>
+  
+  <body id="blog-post">
+	  
+	<?php include('../structure/header.php'); ?>
+	
+	<section>
+		<main class="row">
+			<div class="large-12 column">
+				<article class="large-12 column">
+					<h1>Afternoon Tea in London, England</h1>
+					<p>I've been fortunate to experience afternoon tea in some of London's most exquisite venues. Here's my top 5 picks in order of my favourites.</p>
+					
+					<h3>1. Traditional Afternoon Tea At Fortnum &amp; Mason - Piccadilly</h3>
+					<p></p>
+					<figure>
+						<img src="/images/london/TBC.jpg" alt="TBC">
+						<figcaption>TBC</figcaption>
+					</figure>
+	
+					<h3>2. Traditional Afternoon Tea At The Ritz - Piccadilly</h3>
+					<p></p>
+					<figure>
+						<img src="/images/london/TBC.jpg" alt="TBC">
+						<figcaption>TBC</figcaption>
+					</figure>
+					
+					<h3>3. Tale As Old As Time Tea, Town House At The Kensington - Kensington</h3>
+					<p></p>
+					<figure>
+						<img src="/images/london/TBC.jpg" alt="TBC">
+						<figcaption>TBC</figcaption>
+					</figure>
+
+					<h3>4. Sparkling Afternoon Tea, Kingsway Hall Hotel - Covent Garden</h3>
+					<p></p>
+					<figure>
+						<img src="/images/london/TBC.jpg" alt="TBC">
+						<figcaption>TBC</figcaption>
+					</figure>
+
+					<h3>5. Afternoon Tea at Drink, Shop, Do - Kings Cross</h3>
+					<p></p>
+					<figure>
+						<img src="/images/london/TBC.jpg" alt="TBC">
+						<figcaption>TBC</figcaption>
+					</figure>																			
+					<hr>
+					<h2>My Top Tips</h2>
+					<ul>
+						<li>Food glorious food - if you book the last seating, usually around 7pm you'll normally get extra sandwiches and cakes</li>
+						<li>All about ambience - there's nothing quite like afternoon tea during Christmas and most venues add a festive twist to their menus</li>
+					</ul>
+					<hr>
+					<div class="large-12 column text-right">
+						<a class="button small" href="../holiday-guides/banff">View Banff Guide</a>
+					</div>				
+				</article>
+			</div>
+		</main>
+	</section>
+	
+	<?php include('../structure/footer.php'); ?>
+	
+  </body>
+  <?php include('../structure/scripts.php'); ?>
+</html>
