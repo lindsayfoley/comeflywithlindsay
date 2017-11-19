bd3740d2310f2006d16f20bf610d33ec7142a251
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-19 18:20:25 +0000

    - Added image - Completed F&M section

--- public/holiday-guides/afternoon-tea-london.php
+++ public/holiday-guides/afternoon-tea-london.php
@@ -17,37 +17,38 @@
 			<div class="large-12 column">
 				<article class="large-12 column">
 					<h1>Afternoon Tea in London, England</h1>
-					<p>I've been fortunate to experience afternoon tea in some of London's most exquisite venues. Here's my top 5 picks in order of my favourites.</p>
+					<p>I've been fortunate to experience afternoon tea in some of London's most exquisite venues. Here are my top 5 picks, ranked in order of my preference.</p>
 					
-					<h3>1. Traditional Afternoon Tea At Fortnum &amp; Mason - Piccadilly</h3>
-					<p></p>
+					<h3>1. Traditional Afternoon Tea At Fortnum &amp; Mason - Mayfair</h3>
+					<p>The Diamond Jubilee Tea Salon is just lovely and is the perfect setting to enjoy a luxurious afternoon tea, featuring exquisite food and outstanding 5-star service. There was a great selection of teas and sandwiches which I enjoyed, but the sweet treats are really the showstoppers here. The scones were delicious and served with lemon curd, clotted cream and strawberry jam and the top tier of the cake stand featured rich, flawless looking pastries. The tea salon has a tranquil atmosphere, with live music being played on the grand piano - you can easily forget you're dining in a busy department store in the heart of London! At the end of our tea, our waiter very kindly packed up our lemon curd, jam, leftover pastries and offered us slices of cakes that we were too full try, so we could enjoy these at home.</p>
+					<p>Tea is priced at &pound;44 pp and in my opinion is positively worth it.</p>
 					<figure>
-						<img src="/images/london/TBC.jpg" alt="TBC">
-						<figcaption>TBC</figcaption>
+						<img src="/images/london/traditional-afternoon-tea-fortnum-and-mason.jpg" alt="Afternoon tea at Fortnum & Mason">
+						<figcaption>Afternoon tea at Fortnum &amp; Mason</figcaption>
 					</figure>
 	
-					<h3>2. Traditional Afternoon Tea At The Ritz - Piccadilly</h3>
-					<p></p>
+					<h3>2. Traditional Afternoon Tea At The Ritz - Mayfair</h3>
+					<p>Dining is such a luxurious setting. Service was excellent, food was delicious and we were offered more of everything. Lavish flower arrangements, stunning china and a nice atmosphere</p>
 					<figure>
-						<img src="/images/london/TBC.jpg" alt="TBC">
-						<figcaption>TBC</figcaption>
+						<img src="/images/london/traditional-afternoon-tea-the-ritz.jpg" alt="Afternoon Tea at The Ritz">
+						<figcaption>Afternoon Tea at The Ritz</figcaption>
 					</figure>
 					
 					<h3>3. Tale As Old As Time Tea, Town House At The Kensington - Kensington</h3>
-					<p></p>
+					<p>My favourite childhood Disney film. Beautiful hotel, stunning decor with a cosy touch, Great service. Surprisingly, there were no scones! No scones at an afternoon tea?!</p>
 					<figure>
-						<img src="/images/london/TBC.jpg" alt="TBC">
-						<figcaption>TBC</figcaption>
+						<img src="/images/london/tale-as-old-as-time-tea-town-house-at-the-kensington.jpg" alt="Beauty and The Beast themed afternoon tea at Town House At The Kensington">
+						<figcaption>Beauty and The Beast themed afternoon tea at Town House At The Kensington</figcaption>
 					</figure>
 
 					<h3>4. Sparkling Afternoon Tea, Kingsway Hall Hotel - Covent Garden</h3>
 					<p></p>
 					<figure>
 						<img src="/images/london/TBC.jpg" alt="TBC">
-						<figcaption>TBC</figcaption>
+						<figcaption>Afternoon tea at Kingsway Hall Hotel</figcaption>
 					</figure>
 
-					<h3>5. Afternoon Tea at Drink, Shop, Do - Kings Cross</h3>
+					<h3>5.TBC</h3>
 					<p></p>
 					<figure>
 						<img src="/images/london/TBC.jpg" alt="TBC">
