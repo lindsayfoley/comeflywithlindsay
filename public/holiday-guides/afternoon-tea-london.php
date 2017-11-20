70e4e7cca10fd8c484a046972b371d5cda831f34
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-20 22:55:01 +0000

    - WIP

--- public/holiday-guides/afternoon-tea-london.php
+++ public/holiday-guides/afternoon-tea-london.php
@@ -3,7 +3,7 @@
   <head>
 	<title>Guide To London's Afternoon Tea Scene, England UK | Come Fly With Lindsay</title>
 	<?php include('../structure/meta.php'); ?>
-	<meta name="description" content="I've been to so many lovely traditional afternoon teas in my home city of London, England. Here are my top 5 venues including Fortum and Mason and The Ritz">
+	<meta name="description" content="I've been to so many lovely traditional afternoon teas in my home city of London, England. Here are my top 5 venues including Fortnum and Mason and The Ritz">
 	<?php include('../structure/styles.php'); ?>
 	<?php include('../structure/icons.php'); ?>
   </head>
@@ -17,30 +17,37 @@
 			<div class="large-12 column">
 				<article class="large-12 column">
 					<h1>Afternoon Tea in London, England</h1>
-					<p>I've been fortunate to experience afternoon tea in some of London's most exquisite venues. Here are my top 5 picks, ranked in order of my preference.</p>
+					<p>I've been fortunate to experience traditional afternoon tea in some of London's most exquisite venues. With so many options available, I've put together this little guide to showcase my top picks, listed in order of my preference to provide you with a helping hand!</p>
+					<hr>
 					
-					<h3>1. Traditional Afternoon Tea At Fortnum &amp; Mason - Mayfair</h3>
-					<p>The Diamond Jubilee Tea Salon is just lovely and is the perfect setting to enjoy a luxurious afternoon tea, featuring exquisite food and outstanding 5-star service. There was a great selection of teas and sandwiches which I enjoyed, but the sweet treats are really the showstoppers here. The scones were delicious and served with lemon curd, clotted cream and strawberry jam and the top tier of the cake stand featured rich, flawless looking pastries. The tea salon has a tranquil atmosphere, with live music being played on the grand piano - you can easily forget you're dining in a busy department store in the heart of London! At the end of our tea, our waiter very kindly packed up our lemon curd, jam, leftover pastries and offered us slices of cakes that we were too full try, so we could enjoy these at home.</p>
-					<p>Tea is priced at &pound;44 pp and in my opinion is positively worth it.</p>
+					<h3>Traditional Afternoon Tea At Fortnum &amp; Mason - Mayfair</h3>
+					<p>The Diamond Jubilee Tea Salon is just lovely and is the perfect setting to enjoy a luxurious afternoon tea, featuring exquisite food and outstanding 5-star service. We were given a great selection of teas and sandwiches which I enjoyed, but the sweet treats are really the showstoppers here. The scones were delicious and served with lemon curd, clotted cream and strawberry jam and the top tier of the cake stand featured rich, flawless looking pastries. The tea salon has a tranquil atmosphere, with live music being played on the grand piano - you can easily forget you're dining in a busy department store in the heart of London! At the end of our tea, our waiter very kindly packed up our lemon curd, jam, leftover pastries and offered us slices of cakes that we were too full try, so we could enjoy these at home.</p>
+					<p>The <a target="_blank" href="https://www.fortnumandmason.com/restaurants/afternoon-tea">Fortnum &amp; Mason</a> tea is &pound;44 per person and is more laid back than tea at The Ritz/Dorchester, making it perfect for all occasions.</p>
 					<figure>
 						<img src="/images/london/traditional-afternoon-tea-fortnum-and-mason.jpg" alt="Afternoon tea at Fortnum & Mason">
 						<figcaption>Afternoon tea at Fortnum &amp; Mason</figcaption>
 					</figure>
-	
-					<h3>2. Traditional Afternoon Tea At The Ritz - Mayfair</h3>
-					<p>Dining is such a luxurious setting. Service was excellent, food was delicious and we were offered more of everything. Lavish flower arrangements, stunning china and a nice atmosphere</p>
-					<figure>
-						<img src="/images/london/traditional-afternoon-tea-the-ritz.jpg" alt="Afternoon Tea at The Ritz">
-						<figcaption>Afternoon Tea at The Ritz</figcaption>
-					</figure>
+					<hr>
 					
-					<h3>3. Tale As Old As Time Tea, Town House At The Kensington - Kensington</h3>
-					<p>My favourite childhood Disney film. Beautiful hotel, stunning decor with a cosy touch, Great service. Surprisingly, there were no scones! No scones at an afternoon tea?!</p>
+					<h3>Traditional Afternoon Tea At The Ritz - Mayfair</h3>
+					<p>It was a joy to dine in such an elegant setting with striking floral arrangements and opulent chandeliers which  perfectly complimented the Ritz' Renaissance decor. The sandwiches here were delicious and moreish, we easily made our way through 2 plates of these! Both scones and cakes were light and very tasty, and we were offered a second helping of these dishes. The service was great, a bit fastidious at times and not as relaxed as Fortnum &amp; Mason, but overall it was a lovely, memorable dining experience.</p>
+					<p>Afternoon tea at <a target="_blank" href="https://www.theritzlondon.com/dine-with-us/afternoon-tea/">The Ritz</a> is priced at &pound;54 per person and is ideal for celebrating special occasions or purchased for a loved one as a gift.</p>
+					<figure>
+						<img src="/images/london/traditional-afternoon-tea-the-ritz.jpg" alt="Traditional Afternoon Tea at The Ritz">
+						<figcaption>Traditional Afternoon Tea at The Ritz</figcaption>
+					</figure>
+					<hr>
+					
+<!--
+					<h3>Tale As Old As Time Tea, Town House At The Kensington - Kensington</h3>
+					<p>I was so excited to secure a booking at this extremely popular tea! Being themed around my favourite childhood Disney film, I had high hopes for the menu and was interested to see how the execution would come together. Upon arrival, my first thoughts were how beautiful the Town House hotel was, the decor was tasteful and the tea room was so cosy. The service was good and the waiter was engaging and explained each food item to us well. Surprisingly, there were no scones, no scones at an afternoon tea which I thought was the major downfall of this tea.</p>
 					<figure>
 						<img src="/images/london/tale-as-old-as-time-tea-town-house-at-the-kensington.jpg" alt="Beauty and The Beast themed afternoon tea at Town House At The Kensington">
 						<figcaption>Beauty and The Beast themed afternoon tea at Town House At The Kensington</figcaption>
 					</figure>
-
+					<hr>
+-->
+<!--
 					<h3>4. Sparkling Afternoon Tea, Kingsway Hall Hotel - Covent Garden</h3>
 					<p></p>
 					<figure>
@@ -55,6 +62,7 @@
 						<figcaption>TBC</figcaption>
 					</figure>																			
 					<hr>
+-->
 					<h2>My Top Tips</h2>
 					<ul>
 						<li>Food glorious food - if you book the last seating, usually around 7pm you'll normally get extra sandwiches and cakes</li>
