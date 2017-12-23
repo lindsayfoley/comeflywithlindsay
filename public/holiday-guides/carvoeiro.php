0e483da4a2909a2153bea0dc3fd3b30d2bad69a4
Lindsay Foley <lindsay.foley@outlook.com>
2017-12-23 00:07:57 +0000

    - new page added for carvoeiro

---
+++ public/holiday-guides/carvoeiro.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+	<title>Carvoeiro Travel Guide, Portugal | Come Fly With Lindsay</title>
+	<?php include('../structure/meta.php'); ?>
+	<meta name="description" content="Travel guide on Carvoeiro in Portugal featuring travel advice and details on where to stay, what to eat and things to see, written by Come Fly With Lindsay">
+	<meta property="og:url" content="http://comeflywithlindsay.com/holiday-guides/carvoeiro">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Travel Guide on Carvoeiro in Portugal | Come Fly With Lindsay">
+	<meta property="og:description" content="Travel guide on Carvoeiro in Portugal featuring travel advice and details on where to stay, what to eat and things to see, written by Come Fly With Lindsay">
+	<meta property="og:image" content="http://comeflywithlindsay.com/images/carvoeiro/tivoli-carvoeiro-at-sunset.jpg">
+	
+	<?php include('../structure/styles.php'); ?>
+	<?php include('../structure/icons.php'); ?>
+  </head>
+  
+  <body id="blog-post">
+	  	  
+	<?php include('../structure/header.php'); ?>
+	
+	<section>
+		<main class="row">
+			<div class="large-12 column">
+				<article class="large-12 column">
+					<h1>Carvoeiro, Portugal</h1>
+					<p>We booked a return journey with <a target="_blank" href="http://www.yellowfishtransfers.com/en/">Yellowfish Transfers</a> to collect us from Faro airport and take us to the Tivoli Carvoeiro Hotel, where we booked a 4 night stay. It was &euro;87 for the roundtrip and was highly worth it! Yellowfish provided an excellent service, we had free WiFi in the cars and friendly, knowledgeable drivers.</p>
+					<figure>
+						<img src="/images/carvoeiro/tivoli-carvoeiro-at-sunset.jpg" alt="The stunning Tivoli Carvoeiro at sunset">
+						<figcaption>The stunning Tivoli Carvoeiro at sunset</figcaption>
+					</figure>
+					
+					<p>The <a target="_blank" href="https://www.minorhotels.com/en/tivoli/tivoli-carvoeiro">Tivoli Carvoeiro</a> is not your average 5* Hotel, it’s location is unbeatable; nested up high within the most stunning golden cliffs, with a private sandy beach, surrounded by turquoise and blue ocean waters.</p>
+					<figure>
+						<img src="/images/carvoeiro/caves-scenery-at-tivoli-carvoeiro.jpg" alt="The impressive cliffs of Tivoli Carvoeiro">
+						<figcaption>The impressive cliffs of Tivoli Carvoeiro</figcaption>
+					</figure>	
+					
+					<p>We were very impressed with the presentation of the hotel, the simplistic and modern room decor and the outstanding service we received made it a truly excellent place to stay. Walking into the lobby and looking unimpeded to the sea and the horizon beyond let’s you know you somewhere truly special.</p>
+					<figure>
+						<img src="/images/carvoeiro/sea-view-room-tivoli-carvoeiro.jpg" alt="Sea View room at Tivoli Carvoeiro">
+						<figcaption>Sea View room at Tivoli Carvoeiro</figcaption>
+					</figure>
+
+					<p>The grounds are beautiful and well maintained, it’s worth spending time wandering around and exploring them. There are rough pathways in both sets of surrounding cliffs which you can hike/climb up to see some epic views. The cliff farthest from the pool was our favourite and a good spot to take in the sunset.</p>
+					<figure>
+						<img src="/images/carvoeiro/caves-and-scenery-at-tivoli-carvoeiro.jpg" alt="Cliffs and beautiful scenery">
+						<figcaption>Cliffs and beautiful scenery</figcaption>
+					</figure>
+
+					<p>Breakfast was just perfect, it was an impressive spread, some may say a feast! We were treated to fresh fruits, cured meats, cheese, smoked salmon, eggs made to order, hot buffet choices, cereals, pastries, cakes and much more. The waiters were highly attentive, they even remembered our food choices from the previous day and were on hand to provide anything we requested.</p>
+					<figure>
+						<img src="/images/carvoeiro/breakfast-at-tivoli-carvoeiro.jpg" alt="The mighty breakfast at Tivoli Carvoeiro">
+						<figcaption>The mighty breakfast at Tivoli Carvoeiro</figcaption>
+					</figure>				
+
+					<p>The location of the Tivoli Carvoeiro Hotel is about a 10 minute walk from the town centre. The town is small but lovely, it has restaurants, cafes, local shops and leads to Carvoeiro beach.</p>
+					<figure>
+						<img src="/images/carvoeiro/carvoeiro-town-and-beach.jpg" alt="Carvoeiro beach in town">
+						<figcaption>Carvoeiro beach in town</figcaption>
+					</figure>	
+
+					<p>We visited 2 main beaches on our trip. Both were fairly small but nice, gold sandy beaches, surrounded by picturesque cliffs and caves.</p>
+					<figure>
+						<img src="/images/carvoeiro/carvoeiro-town-and-beach.jpg" alt="Carvoeiro beach">
+						<figcaption>Carvoeiro beach</figcaption>
+					</figure>	
+
+					<p>The boardwalk looks like it goes on forever but it’s not bad at all once your on it so don’t be put off by the length of it! It’s a nice, short stroll along the ocean and leads to the town centre by the beach.</p>
+					<figure>
+						<img src="/images/carvoeiro/carvoeiro-boardwalk-faro-portugal.jpg" alt="Carvoeiro boardwalk">
+						<figcaption>Carvoeiro boardwalk</figcaption>
+					</figure>	
+
+					<p>About mid-way through on the boardwalk you’ll notice some stairs tailing off, follow this to find the caves. There are steps cut into the cliffs that are perfectly fine to use but be warned it does look a bit scary and is unsuitable for small children and those wobbly on their feet. Once you get down to the caves, you can have a good wander and you may choose to climb up higher or follow the pathways cut into the rocks. It’s the perfect place to watch the sunset but make sure to get back up to the boardwalk before it gets dark as there are no lights!</p>
+					<figure>
+						<img src="/images/carvoeiro/views-at-carvoeiro-caves.jpg" alt="The amazing Carvoeiro Caves">
+						<figcaption>The amazing Carvoeiro Caves</figcaption>
+					</figure>		
+
+					<p>We ate out in the town every day for dinner. Villa Medici was our favourite restaurant, offering delicious Italian cuisine and generous portions. The pizza is excellent, has a thin base and huge! We also ate at Wolf’s which was a nice option with a good menu, I highly recommend the tempura prawns to start with. On our last day we visited Case Algarvia to try some Portuguese dishes. We had the pork and clam Cataplana which was like a cross between a stew and a paella. It was tasty but a bit gritty due to the clams. Drinks were very cheap here, with a glass of wine costing €3.50 so a good option if you’re on budget.</p>
+					<figure>
+						<img src="/images/carvoeiro/pizza-villa-medici-carvoeiro.jpg" alt="Pizza at Villa Medici, Carvoeiro">
+						<figcaption>Pizza at Villa Medici, Carvoeiro</figcaption>
+					</figure>
+
+					<p>I recommend a trip to Carvoeiro if you’re in need of a rest or just love to be surrounded by natural beauty. It offers cliffs, caves and scenic oceanic views and is the perfect place to visit in the off-peak season. We visited in December 2017 and the weather was lovely in the day with highs of 18 degrees (pretty hot in the sun) and chilly in the evening. The hotels, town, restaurants and beaches are quiet and peaceful so you can have a truly relaxing experience.</p>
+					
+					<hr>
+					<h2>My Top Tips</h2>
+					<ul>
+						<li>Venture out - if you get bored in Carvoeiro you may be tempted to visit Portimão... don’t! It’s run down and we did not enjoy our time there</li>
+						<li>Sunset viewing - the caves and the boardwalk are definitely the prime spots to watch the sun go down and to get some spectacular pictures</li>
+						<li>Watch your step - there are stairs carved into the rocks at the caves but some steps have eroded away, so take care when making your way down!</li>
+					</ul>
+					<hr>
+					
+					<div class="large-12 medium-12 small-12 column">
+						<?php include('../structure/social.php') ?>
+						<div class="large-6 medium-6 small-6 column text-right">
+						<a class="button small" href="../holiday-guides/banff">Read About Banff</a>
+						</div>
+					</div>	
+				</article>
+			</div>
+		</main>
+	</section>
+	
+	<?php include('../structure/footer.php'); ?>
+	
+  </body>
+  <?php include('../structure/scripts.php'); ?>
+</html>
