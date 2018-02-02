735b3c4c029acb4ed1c65d0239a502d7646f9d76
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-02 23:48:19 +0000

    - Added template folder to store MD and webpage code - Afternoon tea page is now using the parsed MD

---
+++ public/holiday-guides/template-post/blogpost-template-webpage.php
@@ -0,0 +1,18 @@
+<?php 
+	$metaTitle = "London Afternoon Tea Guide";
+	$metaDescription = "A guide to my top traditional afternoon tea venues in London, England with tea reviews of Fortnum and Mason, The Ritz & more, written by Come Fly With Lindsay.";
+	$ogImageUrl = "/images/london/traditional-afternoon-tea-fortnum-and-mason.jpg";		  
+	include($_SERVER['DOCUMENT_ROOT'] . "/structure/head.php");
+	include($_SERVER['DOCUMENT_ROOT'] . "/structure/blog-post-start.php");
+	
+	// Blog post body
+	require_once '../vendor/autoload.php';
+	$Parsedown = new Parsedown();
+	$text = file_get_contents('../holiday-guides/md/blogpost-template.txt');
+	echo $Parsedown->text($text);
+	// /Blog post body
+	
+	$viewMoreLink = "../destinations";
+	$viewMoreText = "View My Holiday Guides";
+	include($_SERVER['DOCUMENT_ROOT'] . "/structure/blog-post-end.php"); 
+?>
