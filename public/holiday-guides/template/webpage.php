a38244e8a38d1f6ab28b2451617a07867be696db
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-04 21:24:53 +0000

    - Minor tweaks to MD files - Made templates more generic

--- public/holiday-guides/template/webpage.php
+++ public/holiday-guides/template/webpage.php
@@ -1,16 +1,15 @@
 <?php 
-	$newPost = 'paris';
-	$newPostFile = fopen($newPost . '.txt', 'w') 
+	$fileName = 'paris';
 	$metaTitle = '';
 	$metaDescription = '';
-	$ogImageUrl = '/images/';		  
+	$ogImageUrl = '/images/' . $destination . '.jpg';		  
 	include($_SERVER['DOCUMENT_ROOT'] . '/structure/head.php');
 	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-start.php');
 	
 	// Blog post body
 	require_once '../vendor/autoload.php';
 	$Parsedown = new Parsedown();
-	$text = file_get_contents('../holiday-guides/md/' . $newPost . '.txt');
+	$text = file_get_contents('../holiday-guides/md/' . $fileName . '.txt');
 	echo $Parsedown->text($text);
 	// /Blog post body
 	
