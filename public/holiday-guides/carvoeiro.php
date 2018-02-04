7b36d76d2de8c98ffe533eca883a07d04eecc75e
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-04 20:40:25 +0000

    All blog posts are now rendered from parsed MD

--- public/holiday-guides/carvoeiro.php
+++ public/holiday-guides/carvoeiro.php
@@ -1,81 +1,19 @@
 <?php 
 	$metaTitle = "Carvoeiro, Portugal Travel Guide";
 	$metaDescription = "Travel guide on Carvoeiro in Portugal featuring travel advice and details on where to stay, what to eat and things to see, written by Come Fly With Lindsay.";
-	$ogImageUrl = "/images/carvoeiro/tivoli-carvoeiro-at-sunset.jpg";		  
+	$ogImageUrl = "/images/carvoeiro/tivoli-carvoeiro-at-sunset.jpg";	 
 	include($_SERVER['DOCUMENT_ROOT'] . "/structure/head.php");
 	include($_SERVER['DOCUMENT_ROOT'] . "/structure/blog-post-start.php");
-?>
-
-<h1>Carvoeiro, Portugal</h1>
-<p>I was ready to travel again and this time I wanted a destination which offered winter sun without being too far from the UK. Portugal looked promising with highs of 19&deg;C and plenty of sunshine hours, so I began researching areas to spend a few nights. I stumbled upon Carvoeiro which looked ideal and as we were travelling in December I thought it would be pretty quiet - and boy was I right! Carvoeiro is a small town in the Algarve which is very close to Lagao and just a 50 minute drive from Faro Airport.</p>
-<figure>
-	<img src="/images/carvoeiro/carvoeiro-town-and-beach.jpg" alt="The quaint town of Carvoeiro">
-	<figcaption>The quaint town of Carvoeiro</figcaption>
-</figure>
-
-<p>We flew into Faro and booked a return journey with <a target="_blank" href="http://www.yellowfishtransfers.com/en/">Yellowfish Transfers</a> to take us to our hotel which would be home for the next 4 nights. The roundtrip cost &euro;87 (discounted for off season) and was worth it as we had free WiFi and friendly, knowledgable drivers.</p>
-<p>The <a target="_blank" href="https://www.minorhotels.com/en/tivoli/tivoli-carvoeiro">Tivoli Carvoeiro Algarve Resort</a> is not your average 5-star Hotel, it’s location is just unbeatable; nested up high within the most stunning ochre coloured cliffs, with a private sandy beach, surrounded by turquoise and blue ocean waters.</p>
-<figure>
-	<img src="/images/carvoeiro/tivoli-carvoeiro-at-sunset.jpg" alt="The stunning Tivoli Carvoeiro Algarve Resort at sunset">
-	<figcaption>The stunning Tivoli Carvoeiro Algarve Resort at sunset</figcaption>
-</figure>	 
-
-<p>We were extremely impressed with the hotel; the simplistic room decor and outstanding service received made it a truly excellent place to stay.</p>
-<figure>
-	<img src="/images/carvoeiro/sea-view-room-tivoli-carvoeiro.jpg" alt="Our room at Tivoli Carvoeiro">
-	<figcaption>Our room at Tivoli Carvoeiro</figcaption>
-</figure>
-
-<p>Walking into the lobby and looking unimpeded to the sea and the horizon beyond lets you know you somewhere truly special. The grounds are breathtaking and very well maintained so it’s worth exploring the area. There are rough pathways in both surrounding cliffs which you can climb up to see some epic views. The cliff opposite the pool was the perfect patch to take in the sunset.</p>
-<figure>
-	<img src="/images/carvoeiro/caves-and-scenery-at-tivoli-carvoeiro.jpg" alt="Cliffs and beautiful scenery around the Tivoli Carvoeiro Resort">
-	<figcaption>Cliffs and beautiful scenery around the Tivoli Carvoeiro Resort</figcaption>
-</figure>
-
-<p>Breakfast was flawless, it was an impressive spread, some may say a feast! We were treated to fresh fruits, cured meats, cheese, smoked salmon, eggs made to order, hot buffet options, cereals, pastries, cakes and much more. The waiters were highly attentive, they even remembered our food choices from the previous day and were on hand to provide anything we requested.</p>
-<figure>
-	<img src="/images/carvoeiro/breakfast-at-tivoli-carvoeiro.jpg" alt="The mighty breakfast at the Tivoli Carvoeiro Resort">
-	<figcaption>The mighty breakfast at the Tivoli Carvoeiro Resort</figcaption>
-</figure>				
-
-<p>The location of the Tivoli Carvoeiro Resort is about a 10 minute walk from the town centre. The town is small but lovely, it has restaurants, cafes, local shops and leads to Carvoeiro beach (Praia do Carvoeiro). We visited 2 main beaches on our trip and both were fairly small but charming with gold sandy beaches, surrounded by picturesque cliffs and caves.</p>
-<figure>
-	<img src="/images/carvoeiro/carvoeiro-beach.jpg" alt="Praia do Carvoeiro">
-	<figcaption>Praia do Carvoeiro</figcaption>
-</figure>	
-
-<p>The boardwalk looks like it goes on forever but it’s not bad at all once you're on it - so don’t be put off by the length of it as you see it twisting and turning in the distance! It’s a pleasant, short stroll along the ocean and leads to the town centre by the beach.</p>
-<figure>
-	<img src="/images/carvoeiro/carvoeiro-boardwalk-faro-portugal.jpg" alt="Carvoeiro boardwalk">
-	<figcaption>Carvoeiro boardwalk</figcaption>
-</figure>	
-
-<p>About mid-way through the boardwalk you’ll notice some stairs tailing off, follow this and you'll find the caves. There are steps cut into the rock formations that are perfectly fine to use, but be warned it does look scary and is unsuitable for small children and those wobbly on their feet. When you reach the caves, you'll see it's the optimal place to watch the sunset... just make sure to get back up to the boardwalk before its dark as there are no lights!</p>
-<figure>
-	<img src="/images/carvoeiro/views-at-carvoeiro-caves.jpg" alt="Neil at the amazing Carvoeiro Caves at sunset">
-	<figcaption>Neil at the amazing Carvoeiro Caves at sunset</figcaption>
-</figure>		
-
-<p>We ate out in the town every day for dinner and Villa Medici was our favourite restaurant, they served up delicious Italian cuisine and generous portions (just look at that pizza!). We also ate at The Wolf Bar &amp; Grill which was a nice option with a small menu, I highly recommend the tempura prawns to start with. On our last day we visited Case Algarvia to try some authentic Portuguese dishes. We tried the pork and clam Cataplana which was like a cross between a stew and a paella, it was tasty but a bit gritty due to the clams. Drinks were especially cheap here, with a glass of wine costing only &euro;3.50 so a very good option for those on a budget.</p>
-<figure>
-	<img src="/images/carvoeiro/pizza-villa-medici-carvoeiro.jpg" alt="Pizza at Villa Medici, Carvoeiro">
-	<figcaption>Pizza at Villa Medici, Carvoeiro</figcaption>
-</figure>
-
-<p>I recommend a trip to Carvoeiro if you’re in need of a rest or just love to be surrounded by natural beauty. It offers cliffs, caves and scenic oceanic views and is the best place to visit in the off-peak season. We visited in December 2017 and the weather was lovely in the day with highs of 18 degrees (surprisingly hot in the sun) and chilly in the evening. The hotels, town, restaurants and beaches are pretty vacant so you can have a truly relaxing experience.</p>
-
-<hr>
-<h2>My Top Tips</h2>
-<ul>
-	<li>Buy now, save later - book transfers in advance to avoid being ripped off! Ask your hotel what companies they recommend</li>
-	<li>Venture out - if you get bored in Carvoeiro you may be tempted to visit Portimão... don’t! It’s rundown and we didn't enjoy it there</li>
-	<li>Sunset sky - the caves and boardwalk are definitely the prime spots to watch the sun go down and to get some spectacular pictures</li>
-	<li>Watch your step - stairs are carved into the rocks at the caves but some have eroded away, so take care when making your way down!</li>
-</ul>
-<hr>
-<?php 
+	
+	// Blog post body
+	require_once '../vendor/autoload.php';
+	$Parsedown = new Parsedown();
+	$text = file_get_contents('../holiday-guides/md/carvoeiro.txt');
+	echo $Parsedown->text($text);
+	// /Blog post body
+	
 	$viewMoreLink = "../banff";
-	$viewMoreText = "Read About Banff";	
+	$viewMoreText = "View Banff Guide";	
 	include($_SERVER['DOCUMENT_ROOT'] . "/structure/blog-post-end.php"); 
 ?>
 
