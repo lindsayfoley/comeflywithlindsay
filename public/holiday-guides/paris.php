e560a3cd4fd6c9330fef2065693ba1256e56d9a9
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-11 17:11:51 +0000

    Adding guide to Paris

---
+++ public/holiday-guides/paris.php
@@ -0,0 +1,18 @@
+<?php 
+	$metaTitle = 'A Travel Guide to Paris, France';
+	$metaDescription = 'Your travel guide to France\'s capital city, Paris! Including where to stay, top attractions to visit and places to eat and drink by Come Fly With Lindsay.';
+	$ogImageUrl = '/images/paris/eiffel-tower-paris-at-sunset.jpg';
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/head.php');
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-start.php');
+	
+	// Blog post body
+	require_once '../vendor/ autoload.php';
+	$Parsedown = new Parsedown();
+	$text = file_get_contents('../holiday-guides/md/paris.txt');
+	echo $Parsedown->text($text);
+	// /Blog post body
+	
+	$viewMoreLink = '../carvoeiro';
+	$viewMoreText = 'Read About Carvoeiro';
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-end.php'); 
+?>
