8e5d2fa9ecf77e093cd587024568ade6d7d572ca
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-14 23:11:18 +0000

    Incremented CSS file due to updating footer styles

--- public/structure/styles.php
+++ public/structure/styles.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="/style/all.min-v6.css">
+<link rel="stylesheet" type="text/css" href="/style/all.min-v7.css">
 <link rel="stylesheet" href="/style/libs/foundation-6.3.1.min.css">
 <link href="//fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> 
 <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
