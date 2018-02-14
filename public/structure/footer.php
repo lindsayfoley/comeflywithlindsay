f55417d92a8b49cebf94843a1d09b16a2aaa5876
Lindsay Foley <lindsay.foley@outlook.com>
2018-02-14 23:09:38 +0000

    Footer styling updated

--- public/structure/footer.php
+++ public/structure/footer.php
@@ -1,16 +1,16 @@
 <footer>
 	<div class="row">
-		<div id="social" class="large-2 medium-2 small-12 column">
+		<div class="large-3 medium-3 small-12 column">
 			<a target="_blank" href="//instagram.com/comeflywithlindsay"><svg xmlns="http://www.w3.org/2000/svg"><path d="M21.231 0h-18.462c-1.529 0-2.769 1.24-2.769 2.769v18.46c0 1.531 1.24 2.771 2.769 2.771h18.463c1.529 0 2.768-1.24 2.768-2.771v-18.46c0-1.529-1.239-2.769-2.769-2.769zm-9.231 7.385c2.549 0 4.616 2.065 4.616 4.615 0 2.549-2.067 4.616-4.616 4.616s-4.615-2.068-4.615-4.616c0-2.55 2.066-4.615 4.615-4.615zm9 12.693c0 .509-.413.922-.924.922h-16.152c-.511 0-.924-.413-.924-.922v-10.078h1.897c-.088.315-.153.64-.2.971-.05.337-.081.679-.081 1.029 0 4.079 3.306 7.385 7.384 7.385s7.384-3.306 7.384-7.385c0-.35-.031-.692-.081-1.028-.047-.331-.112-.656-.2-.971h1.897v10.077zm0-13.98c0 .509-.413.923-.924.923h-2.174c-.511 0-.923-.414-.923-.923v-2.175c0-.51.412-.923.923-.923h2.174c.511 0 .924.413.924.923v2.175z" fill-rule="evenodd" clip-rule="evenodd"/></svg></a>
 			<a target="_blank" href="//facebook.com/comeflywithlindsay"><svg xmlns="http://www.w3.org/2000/svg"><g id="surface1"><path style=" " d="M 21.800781 0 L 2.199219 0 C 1 0 0 1 0 2.199219 L 0 21.800781 C 0 23 1 24 2.199219 24 L 12 24 L 12 14 L 9 14 L 9 11 L 12 11 L 12 8 C 12 5.5 13 4 16 4 L 19 4 L 19 7 L 17.699219 7 C 16.800781 7 16 7.800781 16 8.699219 L 16 11 L 20 11 L 19.5 14 L 16 14 L 16 24 L 21.800781 24 C 23 24 24 23 24 21.800781 L 24 2.199219 C 24 1 23 0 21.800781 0 Z "></path></g></svg></a>
-		</div>
-		<div class="large-7 medium-7 small-12 column">
+		</div>		
+		<div class="large-6 medium-6 small-12 column">
 			<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
 			<div id="mc_embed_signup">
 				<form action="https://comeflywithlindsay.us17.list-manage.com/subscribe/post?u=b227b8206a4a9866eab30d0f7&amp;id=92b146d66d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				    <div id="mc_embed_signup_scroll">
-					<label for="mce-EMAIL">Subscribe to my guides: </label>
-					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+					<label for="mce-EMAIL">Get notified about new guides:</label>
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
 				    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b227b8206a4a9866eab30d0f7_92b146d66d" tabindex="-1" value=""></div>
 				    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
@@ -18,8 +18,8 @@
 				</form>
 			</div>
 		</div>
-		<div class="large-3 medium-3 small-12 column text-right">
-			<p>&copy; <a target="_blank" href="http://www.lindsayfoley.co.uk">Lindsay Foley</a></p>
+		<div class="large-3 medium-3 small-12 column">
+			<a target="_blank" href="http://www.lindsayfoley.co.uk">&copy; Lindsay Foley</a>
 		</div>
 	</div>
 </footer>
