8df49d93908c3f0a08eba26f19af994d816c3671
Lindsay Foley <lindsay.foley@outlook.com>
2018-03-17 17:05:56 +0000

    Small CSS tweaks and CSS file name update

--- public/structure/blog-post-end.php
+++ public/structure/blog-post-end.php
@@ -1,4 +1,4 @@
-					<div class="large-12 medium-12 small-12">
+					<div id="post-footer" class="large-12 medium-12 small-12">
 						<?php include($_SERVER['DOCUMENT_ROOT'] . "/structure/social.php"); ?>
 						<div class="large-6 medium-6 small-6 column text-right">
 							<a class="button small" href="<?php echo $viewMoreLink ?>"><?php echo $viewMoreText ?></a>
--- public/structure/styles.php
+++ public/structure/styles.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="/style/all.min-v8.css">
+<link rel="stylesheet" type="text/css" href="/style/all-min-1.0.1.css">
 <link rel="stylesheet" href="/style/libs/foundation-6.3.1.min.css">
 <link href="//fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> 
 <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
