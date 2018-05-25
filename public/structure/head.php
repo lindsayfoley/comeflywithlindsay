8a3ebb289d8e6173af84f5f9c7ca49323d47d2eb
Lindsay Foley <lindsay.foley@outlook.com>
2018-05-25 19:23:35 +0100

    Added Pinterest meta tags

--- public/structure/head.php
+++ public/structure/head.php
@@ -12,6 +12,8 @@
 <meta property="og:url" content="http://<?php echo $domain, $_SERVER['REQUEST_URI'] ?>">
 <meta property="og:image" content="http://<?php echo $domain, $ogImageUrl ?>">
 <meta property="og:type" content="website">
+<meta property="og:type" content="blog">
+<meta property="article:author" content="Come Fly With Lindsay">
 <meta property="fb:app_id" content="479045892495492">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/icon.png">  
@@ -19,6 +21,5 @@
 <link rel="apple-touch-icon" sizes="114x114" href="icon.png">  
 <link rel="apple-touch-icon" sizes="144x144" href="icon.png">	
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/structure/styles.php") ?>
-
 </head>
 
