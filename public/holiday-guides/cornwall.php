81f0b49c97664afa953ddc235aaf7c81b08c45d1
Lindsay Foley <lindsay.foley@outlook.com>
2018-07-03 22:19:43 +0100

    Cornwall post is a WIP

---
+++ public/holiday-guides/cornwall.php
@@ -0,0 +1,18 @@
+<?php
+	$metaTitle = '2 Days Visiting Mawgan Porth In Cornwall, England';
+	$metaDescription = 'I stayed at Mawgan Porth in Newquay, North Cornwall and had no idea that England had such heavenly, sandy beaches with ombre blue waters!';
+	$ogImageUrl = '/images/cornwall/mawgan-porth-beach.jpg';
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/head.php');
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-start.php');
+	
+	// Blog post body
+	require_once '../vendor/autoload.php';
+	$Parsedown = new Parsedown();
+	$text = file_get_contents('../holiday-guides/md/cornwall.txt');
+	echo $Parsedown->text($text);
+	// /Blog post body
+	
+	$viewMoreLink = '../cotswolds';
+	$viewMoreText = 'Cotswolds Travel Guide';
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-end.php'); 
+?>
