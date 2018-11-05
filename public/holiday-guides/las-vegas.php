1d957746923a0c3379d9ef2087aeea11482e0fc5
Lindsay Foley <lindsay.foley@disney.com>
2018-11-05 22:42:26 +0000

    LV post complete!

---
+++ public/holiday-guides/las-vegas.php
@@ -0,0 +1,18 @@
+<?php 
+	$metaTitle = 'My Guide to Las Vegas';
+	$metaDescription = 'A quick guide to help you make the best choices for your time in Las Vegas! I advise on where to eat and the best area and hotels to stay in Vegas.';
+	$ogImageUrl = '/images/las-vegas/las-vegas-strip-at-night.jpg';		  
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/head.php');
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-start.php');
+	
+	// Blog post body
+	require_once '../vendor/autoload.php';
+	$Parsedown = new Parsedown();
+	$text = file_get_contents('../holiday-guides/md/las-vegas.txt');
+	echo $Parsedown->text($text);
+	// /Blog post body
+	
+	$viewMoreLink = '../banff';
+	$viewMoreText = 'View Banff Guide';	
+	include($_SERVER['DOCUMENT_ROOT'] . '/structure/blog-post-end.php'); 
+?>
