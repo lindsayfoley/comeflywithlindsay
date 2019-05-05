6fd68c59c8b1e7eea1ac47f56c9064b790545e04
Lindsay Foley <lindsay.foley@disney.com>
2019-05-05 22:13:36 +0100

    Removed blog link from nav

--- public/structure/header.php
+++ public/structure/header.php
@@ -15,7 +15,6 @@
 					  <li><a href="/destinations">Destinations</a></li>
 					  <li><a href="/about">About</a></li>
 					  <li><a href="/contact">Contact</a></li>
-					  <li><a href="/blog">Blog</a></li>
 					</ul>
 			    </nav>
 			</div>
