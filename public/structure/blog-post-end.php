2b527570b2af96ff8e15e624b5c333b4e8b00988
Lindsay Foley <lindsay.foley@charlottetilbury.com>
2019-11-20 01:06:03 +0000

    Add back to top button and added a fix for positioning of in page anchors

--- public/structure/blog-post-end.php
+++ public/structure/blog-post-end.php
@@ -1,3 +1,4 @@
+					<button id="toTop">&#8593;</button>
 					<div id="post-footer" class="large-12 medium-12 small-12">
 						<?php include($_SERVER['DOCUMENT_ROOT'] . "/structure/social.php"); ?>
 						<div class="large-6 medium-6 small-6 column text-right">
