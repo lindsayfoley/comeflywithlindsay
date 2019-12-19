9a34d9426018ff46a9dbdf31887545254e1151f9
Lindsay Foley <lindsay.foley@charlottetilbury.com>
2019-12-19 10:43:18 +0000

    Removed CJ and Expedia links, replacing with Booking.com

--- public/structure/blog-post-end.php
+++ public/structure/blog-post-end.php
@@ -7,11 +7,6 @@
 					</div>
 				</div>
 			</article>
-			<hr>
-			<div id="searchWidget" class="large-12 medium-12 small-12 column">
-				<h3>Plan Your Next Adventure</h3>
-				<iframe id="widgetIframe" src="https://www.expedia.co.uk/marketing/widgets/searchform/widget?wtt=6&tp1=467715&tp2=&lob=H,FH,F,CR&des=&wbi=8&olc=000000&whf=4&hfc=C7C7C7&wif=4&ifc=000000&wbc=FFB647&wbf=4&bfc=3D3100&wws=2&sfs=H100RW270F&langid=2057" style="width:100%; height:270px;" scrolling="no" frameborder="0"></iframe>
-			</div>
 		</main>
 	</section>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/structure/footer.php"); ?>
