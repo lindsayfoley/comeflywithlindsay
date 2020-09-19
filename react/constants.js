6165f4bc545e9da8c30825fd61ca1eb44cebdcf4
Lindsay Foley <lindsay.foley@outlook.com>
2020-09-19 13:31:48 +0100

    replacing old site with new React app

--- react/constants.js
+++
@@ -1,9 +0,0 @@
-export const DOMAIN = "https://comeflywithlindsay.com/";
-export const BLOG_POST_SOCIAL_ICON_SIZE = "35px";
-export const FOOTER_SOCIAL_ICON_SIZE = "32px";
-export const COME_FLY_WITH_LINDSAY_INSTAGRAM = "https://www.instagram.com/comeflywithlindsay/";
-export const COME_FLY_WITH_LINDSAY_PINTEREST = "https://www.pinterest.co.uk/comeflywithlindsay";
-export const COME_FLY_WITH_LINDSAY_FACEBOOK = "https://www.facebook.com/comeflywithlindsay";
-export const PORTFOLIO_SITE = "https://lindsayfoley.co.uk/";
-export const EMAIL_ADDRESS = "mailto:me@indsayfoley.co.uk";
-export const NEWSLETTER_SUBSCRIBE_URL = "https://comeflywithlindsay.us17.list-manage.com/subscribe/post?u=b227b8206a4a9866eab30d0f7&amp;id=92b146d66d";
--- react/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm i && npm run build
-cp -r out/* /home/lindsay/public/comeflywithlindsay.com/public
-ls /home
