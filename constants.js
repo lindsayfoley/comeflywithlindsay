47415eb4e7df0eb9b89d1c72b5464ad17f62d229
Lindsay Foley <lindsay.foley@outlook.com>
2020-11-15 12:46:14 +0000

    small tweak to HR, removing trailing slashes

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
-export const DOMAIN = "https://comeflywithlindsay.com/";
+export const DOMAIN = "https://comeflywithlindsay.com";
 export const BLOG_POST_SOCIAL_ICON_SIZE = "40px";
 export const FOOTER_SOCIAL_ICON_SIZE = "35px";
-export const COME_FLY_WITH_LINDSAY_INSTAGRAM = "https://www.instagram.com/comeflywithlindsay/";
+export const COME_FLY_WITH_LINDSAY_INSTAGRAM = "https://www.instagram.com/comeflywithlindsay";
 export const COME_FLY_WITH_LINDSAY_PINTEREST = "https://www.pinterest.co.uk/comeflywithlindsay";
 export const COME_FLY_WITH_LINDSAY_FACEBOOK = "https://www.facebook.com/comeflywithlindsay";
-export const PORTFOLIO_SITE = "https://lindsayfoley.co.uk/";
+export const PORTFOLIO_SITE = "https://lindsayfoley.co.uk";
 export const EMAIL_ADDRESS = "mailto:me@indsayfoley.co.uk";
 export const NEWSLETTER_SUBSCRIBE_URL = "https://comeflywithlindsay.us17.list-manage.com/subscribe/post?u=b227b8206a4a9866eab30d0f7&amp;id=92b146d66d";
 export const FACEBOOK_APP_ID = "661283638133674";
