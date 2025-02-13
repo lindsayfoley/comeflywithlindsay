c37f9dd28f6eb75b64d7820897428a8ba5a71b12
Lindsay Foley <lindsay.foley@outlook.com>
2025-02-13 21:13:56 +0000

    adjusting padding on blog meta info and adding new pics for Bologna

--- components/PostDetails.tsx
+++ components/PostDetails.tsx
@@ -18,7 +18,13 @@ const PostDetails = ({
           text-transform: uppercase;
           color: #535252;
           font-size: 13px;
+          margin-top: 5px;
           margin-bottom: 15px;
+        }
+
+         @media all and (max-width: 667px) {
+          span {
+            margin-top: 10px;
         }
       `}</style>
     </>
